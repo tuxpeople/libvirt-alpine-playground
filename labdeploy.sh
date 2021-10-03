0d8c5b645ebf49dae104c92e968a4ffa1ec67179
Thomas Deutsch <thomas@tuxpeople.org>
2021-10-03 17:01:09 +0200

    add labdeply.sh

---
+++ labdeploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+yum -y install jq
+
+kvm-install-vm remove alpine-playground
+
+VMDIR="/data/virt/vms"
+VMNAME="alpine-playground"
+IMG="$(curl -Ls https://api.github.com/repos/tuxpeople/libvirt-alpine-playground/releases/latest | jq '.assets[].browser_download_url' -r)"
+DISK_SIZE="20G"
+# Create image directory if it doesn't already exist
+mkdir -p ${VMDIR}
+check_vmname_set
+# Start clean
+[ -d "${VMDIR}/${VMNAME}" ] && rm -rf ${VMDIR}/${VMNAME}
+mkdir -p ${VMDIR}/${VMNAME}
+pushd ${VMDIR}/${VMNAME}
+DISK=${VMNAME}.qcow2
+wget ${IMG} -O /data/virt/images/alpine-playground.qcow2
+qemu-img create -q -f qcow2 -F qcow2 -b /data/virt/images/alpine-playground.qcow2 $DISK
+qemu-img resize $DISK $DISK_SIZE
+virsh \
+    pool-create-as \
+    --name=${VMNAME} \
+    --type=dir \
+    --target=${VMDIR}/${VMNAME} \
+
+virt-install \
+    --import \
+    --name=${VMNAME} \
+    --memory=2048 \
+    --vcpus=1 \
+    --cpu=host \
+    --disk=${VMNAME}.qcow2,bus=virtio \
+    --network=bridge=bridge99,model=virtio \
+    --os-variant=alpinelinux3.7 \
+    --noautoconsole \
+    --graphics=spice,port=-1,listen=localhost
